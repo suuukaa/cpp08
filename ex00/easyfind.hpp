3088ec762788339775c57dc47991adc600534c82
Soukaina El Bouyakoubi <151529865+suuukaa@users.noreply.github.com>
2025-05-23 15:10:23 +0000

    function with iterator done

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,16 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+template<typename T>
+typename T::const_iterator easyfind(const T& container, int i){
+    typename T::const_iterator it = std::find(container.begin(), container.end(), i);
+    if (it != container.end())
+        return it;
+    throw std::runtime_error("error");
+}
+
+#endif
