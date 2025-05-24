000b4cfbbea6f71798e904cde2c5f768b80ddad3
Soukaina El Bouyakoubi <151529865+suuukaa@users.noreply.github.com>
2025-05-24 13:23:12 +0000

    done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,6 +1,30 @@
-#ifndef MULTANTSTACH_HPP
-#define MULTANTSTACH_HPP
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>{
+
+    public :
+
+        MutantStack(): std::stack<T>() {}
+        MutantStack(const MutantStack &copy): std::stack<T>(copy){}
+        MutantStack& operator=(const MutantStack &copy){
+            if(this != &copy)
+                std::stack<T>::operator=(copy);
+            return *this;
+        }
+        ~MutantStack(){}
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin(){
+            return this->c.begin();
+        }
+        iterator end(){
+            return this->c.end();
+        }
+};
 
 #endif
